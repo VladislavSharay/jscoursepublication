d8f1107a60693d4406e9b19519f859db087d9208
VladislavSharay <VladislavSharay>
2019-08-19 12:21:30 +0300

    add photo btn

--- profilepage/profilepage.js
+++ profilepage/profilepage.js
@@ -38,48 +38,105 @@ function delete_cookie(name) {
 
 
 // Загрузка фото
-// let urlImg
-// let container = document.querySelector(".container");
-// let addPhotoBtn = document.querySelector('.addPhoto');
-// let imgOut = document.createElement('img');
-// imgOut.id = 'imgOut'; imgOut.width='400'; imgOut.height='400';
-// let formImg = document.forms.namedItem('addPhoto');
+let containerImg = document.getElementById("containerImg");
+let formImg = document.forms.namedItem('addPhotoForm');
 
-// imgOut.style.display='none';
+formImg.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    var formImgD = new FormData(formImg);
 
-// container.appendChild(imgOut)
+    formImgD.append('parentEntityId', 'insta');
+    
+    doRequestImg('POST', formImgD, {'token': cookieToken});
+    containerImg.innerText='';
+    imgOUT(cookieToken)
+})
 
 
-// formImg.addEventListener('submit', function(ev){
-//         var formImgD = new FormData(formImg);
+function doRequestImg(method, data, headers) {
+    fetch('https://intern-staging.herokuapp.com/api/file', {
+        method: method,
+        body: data,
+        headers: headers,
+    }).then(
+        resp => resp.json()
+    ).then(
+        json => {
+            console.log(json)   
+        }
+    );
+}
 
-//         formImgD.append('parentEntityId', '1455qwe1');
 
-//         doRequestImg('POST', formImgD, {
-//                 'token': cookieToken
-//         });
 
-//         ev.preventDefault();
-
-// })
-// function doRequestImg(method, data, headers) {
-//         fetch('https://intern-staging.herokuapp.com/api/file', {
-//             method: method,
-//             body: data,
-//             headers: headers,
-//         }).then(
-//             resp => resp.json()
-//         ).then(
-//             json => {
-//                 console.log(json)
-//                 urlImg = json.url;
-//                  imgOut.src=urlImg;
-//                  imgOut.style.display='block';
+function imgOUT(token) {
+        fetch('https://intern-staging.herokuapp.com/api/file/insta', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'token': token
+            },
+        }).then(
+            resp => resp.json()
+        ).then(
+            json => {   
+               console.log(json)
+               let fragment = new DocumentFragment();
+               for( let i=0; i<json.length; i++){
+                console.log(json[i].url);
+                   let imgObj = document.createElement('img');
+                   imgObj.src=json[i].url;
+                   imgObj.width='400';
+                   imgObj.height='400';
                 
-//             }
-//         );
-//     }
+                   fragment.append(imgObj);
+                }
+                  containerImg.appendChild(fragment);
+            }
+        );
+    }
+    imgOUT(cookieToken)
+   }
+//Кноппка загрузки
 
-  }
+var containerToggle = document.createElement("div");
+containerToggle.id = "container";
+
+let imgBtn=document.querySelector('.imgBtn')
+let fileBtn=document.querySelector('#fileBtn')
+let inpSUB=document.querySelector('#inpSUB')
+
+containerToggle.appendChild(fileBtn);
+containerToggle.appendChild(inpSUB);
+
+imgBtn.innerText = 'Click me';
+imgBtn.style.display="block";
+// containerToggle.style.display = "block";
+// fileBtn.style.display = "none"
+// inpSUB.style.display = "none"
+
+// imgBtn.addEventListener("click", function () {
+//         if (containerToggle.style.display == "none") {
+//             containerToggle.style.display = "block"
+//         } else {
+//             containerToggle.style.display = "none"
+//         }
+//     })
 
 
+   var inputs = document.querySelectorAll('.inpIMG');
+   Array.prototype.forEach.call(inputs, function(input){
+     var label	 = input.nextElementSibling,
+         labelVal = label.innerHTML;
+     input.addEventListener('change', function(e){
+       var fileName = '';
+       if( this.files && this.files.length > 1 )
+       fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+     else
+       fileName = e.target.value.split( '\\' ).pop();
+                 if( fileName )
+       label.innerHTML = fileName;
+     else
+         label.innerHTML = labelVal;
+           });
+   });
